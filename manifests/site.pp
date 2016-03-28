0653725c9f8bd1fb48928525eb237862e19dbf38
msndkar <milind.nadkarni@intel.com>
2016-03-28 15:07:51 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
- { exec "cowsay 'Welcome to ${::fqdn}!' > /etc/motd"}
+ exec { cowsay 'Welcome to ${::fqdn}!' > /etc/motd"}
+ path => '/usr/local/bin' 
 }
