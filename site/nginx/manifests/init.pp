c3e1547f51cba08e45fc6fd1b344ef49917b3474
msndkar <milind.nadkarni@intel.com>
2016-03-31 10:30:04 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,79 +1,39 @@
-class nginx {
-    ##$virt         = capitalize($::virtual)
-    ##notify { "Hello, my name is ${virt}\n": }
-    case $::osfamily {
-        'redhat' : {
-            $package_name = 'nginx'
-            $service_name = 'nginx'
-            $file_owner   = 'root'
-            $file_group   = 'root'
-            $doc_root     = '/var/www'
-            $config_dir   = '/etc/nginx'
-            $svr_blk_dir  = '/etc/nginx/conf.d'
-            $log_dir      = '/var/log/nginx'
-            $usr_run_as   = 'nginx'
-        }
-        'debian' : {
-            $package_name = 'nginx'
-            $service_name = 'nginx'
-            $file_owner   = 'root'
-            $file_group   = 'root'
-            $doc_root     = '/var/www'
-            $config_dir   = '/etc/nginx'
-            $svr_blk_dir  = '/etc/nginx/conf.d'
-            $log_dir      = '/var/log/nginx'
-            $usr_run_as   = 'nginx'
-            
-        }        
-        'windows' : {
-            $package_name = 'nginx-service'
-            $service_name = 'nginx'
-            $file_owner   = 'Administrator'
-            $file_group   = 'Administrators'
-            $doc_root     = 'C:/ProgramData/nginx/html'
-            $config_dir   = 'C:/ProgramData/nginx'
-            $svr_blk_dir  = 'C:/ProgramData/nginx/conf.d'
-            $log_dir      = 'C:/ProgramData/nginx/logs'
-            $usr_run_as   = 'nobody'
-        }        
-        default : {
-            fail("Module ${module_name} Y is not supported on ${::osfamily}")
-        }
-    }
-    $user = $::osfamily ? {
-        'redhat' => 'nginx',
-        'debian' => 'www-data',
-        'windows' => 'nobody',
-    }
-    File {
-    	owner   => $owner,
-    	group   => $group,
-    	mode    => '0664',
-    ##	require => Package[$service_name],
-    ##	notify  => Service[$service_name],    	
-    }
-    package { $package_name:
-    	ensure  => present,
-    }
-    file { [$doc_root, $svr_blk_dir]:
-    	ensure  => directory,
-    }	
-    file { "${doc_root}/index.html":
-    	ensure  => file,
-    	source  => 'puppet:///modules/nginx/index.html',
-    }
-    file { "${config_dir}/nginx.conf":
-    	ensure  => file,
-    	content => template('nginx/nginx.conf.erb'),
-    	notify => Service['nginx'],
-    }
-    file { "${svr_blk_dir}/default.conf":
-    	ensure  => file,
-    	content => template('nginx/default.conf.erb'),
-    	notify => Service['nginx'],    	
-    }
-    service { $service_name:
-    	ensure => running,
-    	enable => true,
-    }
+class nginx (
+$package = $nginx::params::package,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir = $nginx::params::logdir,
+$user = $nginx::params::user,
+) inherits nginx::params {
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
+}
+package { $package:
+ensure => present,
+}
+file { [ $docroot, "${confdir}/conf.d" ]:
+ensure => directory,
+}
+file { "${docroot}/index.html":
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { "${confdir}/nginx.conf":
+ensure => file,
+content => template('nginx/nginx.conf.erb'),
+notify => Service['nginx'],
+}
+file { "${confdir}/conf.d/default.conf":
+ensure => file,
+content => template('nginx/default.conf.erb'),
+notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
 }
